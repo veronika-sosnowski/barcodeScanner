170ae3ac07992c74f60a77ebb5a3334a88da15b4
veronika-sosnowski <veronika.sosnowski@gmail.com>
2014-11-04 16:46:46 -0500

    list

--- www/js/app.js
+++ www/js/app.js
@@ -4,17 +4,14 @@ document.addEventListener("deviceready", init, false);
 function init() {
 	document.querySelector("#startScan").addEventListener("touchend", startScan, false);
 	resultDiv = document.querySelector("#results");
-	lookup("0060410001295");
+	//lookup("0060410001295");
 }
 
 function startScan() {
 
     cordova.plugins.barcodeScanner.scan(
 		function (result) {
-		    var s = "Result: " + result.text + "<br/>" +
-			"Format: " + result.format + "<br/>" +
-			"Cancelled: " + result.cancelled;
-		    resultDiv.innerHTML = s;
+		    lookup(result.text);
 		},
 		function (error) {
 		    alert("Scanning failed: " + error);
@@ -23,5 +20,11 @@ function startScan() {
 }
 function lookup(upc) {
     jQuery.ajax({
-        url: "http://api.upcdatabase.org/json/a076fe196e7054a3da2a2d8ac2685187/" + upc}).done(function (result) { alert("I was here." + result.itemname); });
+        url: "http://api.upcdatabase.org/json/a076fe196e7054a3da2a2d8ac2685187/" + upc
+    }).done(function (result) { displayresult(result.itemname);});
 }
+function displayresult(result) {
+    resultDiv = document.querySelector("#results");
+    resultDiv.innerHTML = resultDiv.innerHTML + "<br>" + result;
+}
+//displayresult("Sun Chips");
