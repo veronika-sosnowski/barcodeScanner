a9d746a8c1e6f9db2272291903111922e392542d
veronika-sosnowski <veronika.sosnowski@gmail.com>
2014-11-04 16:15:55 -0500

    adkfjn

--- www/js/app.js
+++ www/js/app.js
@@ -4,20 +4,24 @@ document.addEventListener("deviceready", init, false);
 function init() {
 	document.querySelector("#startScan").addEventListener("touchend", startScan, false);
 	resultDiv = document.querySelector("#results");
+	lookup("0060410001295");
 }
 
 function startScan() {
 
-	cordova.plugins.barcodeScanner.scan(
+    cordova.plugins.barcodeScanner.scan(
 		function (result) {
-			var s = "Result: " + result.text + "<br/>" +
+		    var s = "Result: " + result.text + "<br/>" +
 			"Format: " + result.format + "<br/>" +
 			"Cancelled: " + result.cancelled;
-			resultDiv.innerHTML = s;
+		    resultDiv.innerHTML = s;
 		},
 		function (error) {
-			alert("Scanning failed: " + error);
+		    alert("Scanning failed: " + error);
 		}
 	);
-
+}
+function lookup(upc) {
+    jQuery.ajax({
+        url: "http://api.upcdatabase.org/json/a076fe196e7054a3da2a2d8ac2685187/" + upc}).done(function (result) { alert("I was here." + result.itemname); });
 }
